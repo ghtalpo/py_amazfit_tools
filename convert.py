2fa3270750e042aca1753a94bc92de131ee25a05
jjkim <gtalpo@gmail.com>
2019-08-19 07:03:56 +0900

    Auto detect resourse type for convert.py
    - experimental BIP support

--- convert.py
+++ convert.py
@@ -1,68 +1,92 @@
 import os
 import logging
 
-original_size = 454 # gtr
-target_size = 360 # verge lite
+class Converter:
+    width_gtr = 454
+    width_verge_lite = 360
+    width_bip = 176
+
+    def __init__(self):
+        self.source_size = 0
+        self.target_size = Converter.width_verge_lite
+
+    def loadJson(self, json_path):
+        try:
+            import json
+            with open(json_path, 'rb') as stream:
+                return json.load(stream)
+
+        except Exception as e:
+            logging.fatal(e, exc_info=True)
+            return None
 
 
-def loadJson(json_path):
-    try:
-        import json
-        with open(json_path, 'rb') as stream:
-            return json.load(stream)
+    def saveJson(self, json_path, tree):
+        try:
+            import json
+            def dumper(obj):
+                try:
+                    return obj.toJSON()
+                except AttributeError:
+                    return obj.__dict__
 
-    except Exception as e:
-        logging.fatal(e, exc_info=True)
-        return None
-
-
-def saveJson(json_path, tree):
-    try:
-        import json
-        def dumper(obj):
-            try:
-                return obj.toJSON()
-            except AttributeError:
-                return obj.__dict__
-
-        with open(json_path, 'w') as stream:
-            stream.write(json.dumps(tree, default=dumper, indent = 2))
-            stream.flush()
-    except Exception as e:
-        logging.fatal(e, exc_info=True)
+            with open(json_path, 'w') as stream:
+                stream.write(json.dumps(tree, default=dumper, indent = 2))
+                stream.flush()
+        except Exception as e:
+            logging.fatal(e, exc_info=True)
 
 
-def patchTree(tree, depth = 0):
-    for k in tree:
-        value = tree[k]
-        if type(value) == int:
-            if k.endswith('X') or k.endswith('Y') or k == 'Spacing':
-                tree[k] = int(value / original_size * target_size)
-        elif type(value) == dict:
-            patchTree(value, depth + 1)
+    def patchTree(self, tree, depth = 0):
+        for k in tree:
+            value = tree[k]
+            if type(value) == int:
+                if k.endswith('X') or k.endswith('Y') or k == 'Spacing':
+                    tree[k] = int(value / self.source_size * self.target_size)
+            elif type(value) == dict:
+                self.patchTree(value, depth + 1)
 
 
-def resizeJson(json_path):
-    tree = loadJson(json_path)
-    patchTree(tree)
-    saveJson(json_path, tree)
+    def resizeJson(self, json_path):
+        tree = self.loadJson(json_path)
+        self.patchTree(tree)
+        self.saveJson(json_path, tree)
 
 
-def resizePng(top_dir):
-    from PIL import Image, ImageDraw
-    for root, _, files in os.walk(top_dir, topdown=False):
-        for name in files:
-            full_path = os.path.join(root, name)
-            if name.endswith('.png'):
-                im = Image.open(full_path)
-                (w, h) = im.size
-                new_w = int(w * target_size / original_size)
-                new_h = int(h * target_size / original_size)
-                im_resized = im.resize((new_w, new_h), resample = Image.LANCZOS)
-                im_resized.save(full_path)
-            elif name.endswith('.json'):
-                resizeJson(full_path)
-    print('Done...', top_dir)
+    def detectRatio(self, top_dir):
+        target_path = os.path.join(top_dir, '0000.png')
+        from PIL import Image
+        im = Image.open(target_path)
+        (w, h) = im.size
+        if w == h:
+            if w == Converter.width_gtr:
+                self.source_size = Converter.width_gtr
+                print('GTR source')
+                return
+            elif w == Converter.width_bip:
+                self.source_size = Converter.width_bip
+                print('BIP source')
+                return
+        raise Exception(f"Unsupported image source dimension({w}/{h})")
+
+
+    def resizePng(self, top_dir):
+        from PIL import Image
+        self.detectRatio(top_dir)
+        for root, _, files in os.walk(top_dir, topdown=False):
+            for name in files:
+                full_path = os.path.join(root, name)
+                if name.endswith('.png'):
+                    im = Image.open(full_path)
+                    (w, h) = im.size
+                    new_w = int(w * self.target_size / self.source_size)
+                    new_h = int(h * self.target_size / self.source_size)
+                    im = im.convert('RGBA')
+                    im_resized = im.resize((new_w, new_h), resample = Image.LANCZOS)
+                    im_resized.save(full_path)
+                elif name.endswith('.json'):
+                    self.resizeJson(full_path)
+        print('Done...', top_dir)
 
 
 if __name__ == '__main__':
@@ -84,4 +108,4 @@ if __name__ == '__main__':
         if not isDirectory:
             print("Not supported yet.")
             sys.exit(1)
-        resizePng(inputFileName)
+        Converter().resizePng(inputFileName)
