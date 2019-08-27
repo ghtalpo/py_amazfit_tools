515a45a5ec97f27502cb927e286d4490cce26dee
jjkim <gtalpo@gmail.com>
2019-08-28 00:53:20 +0900

    Fix incorrect preview size for gtr

--- watchFaceParser/config.py
+++ watchFaceParser/config.py
@@ -1,6 +1,7 @@
 class Config:
     _is_gtr = False
     _image_size = 360
+    _preview_size = 210
 
 
     @staticmethod
@@ -8,6 +9,7 @@ class Config:
         Config._is_gtr = gtr
         if Config._is_gtr:
             Config._image_size = 454
+            Config._preview_size = 266
 
 
     @staticmethod
@@ -23,3 +25,10 @@ class Config:
     @staticmethod
     def getImageSizeHalf():
         return int(Config._image_size / 2)
+
+
+    @staticmethod
+    def getPreviewSize():
+        # return (Config._preview_size, Config._preview_size)
+        return Config._preview_size
+
