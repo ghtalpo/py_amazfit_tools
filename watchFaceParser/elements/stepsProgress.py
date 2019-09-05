47b8b1e2d5487946e1d86acb7c6516cb63958c4d
jjkim <gtalpo@gmail.com>
2019-09-06 07:07:14 +0900

    Test status icons for GTR
    - try to revive battery icons also

--- watchFaceParser/elements/stepsProgress.py
+++ watchFaceParser/elements/stepsProgress.py
@@ -1,7 +1,11 @@
 from watchFaceParser.elements.basicElements.circleScale import CircleScale
+from watchFaceParser.elements.basicElements.imageSet import ImageSet
 
 class StepsProgress:
     definitions = {
+        1: { 'Name': 'Images1', 'Type': ImageSet}, # test
+        2: { 'Name': 'Images2', 'Type': ImageSet}, # test
+        4: { 'Name': 'Images4', 'Type': ImageSet}, # test
         3: { 'Name': 'Circle', 'Type': CircleScale},
         5: { 'Name': 'Unknown5', 'Type': CircleScale}, # verge
     }
--- watchFaceParser/models/elements/stepsProgressElement.py
+++ watchFaceParser/models/elements/stepsProgressElement.py
@@ -5,11 +5,24 @@ from watchFaceParser.models.elements.basic.containerElement import ContainerElem
 class StepsProgressElement(ContainerElement):
     def __init__(self, parameter, parent = None, name = None):
         self._circular = None
+        self._images1 = None
+        self._images2 = None
+        self._images4 = None
         super(StepsProgressElement, self).__init__(parameters = None, parameter = parameter, parent = parent, name = name)
 
 
     def getCircular(self):
         return self._circular
+
+
+    def getImages1(self):
+        return self._images1
+
+    def getImages2(self):
+        return self._images2
+
+    def getImages4(self):
+        return self._images4
 
 
     # def getCircular2(self):
@@ -22,6 +35,18 @@ class StepsProgressElement(ContainerElement):
             from watchFaceParser.models.elements.goalProgress.circularGoalProgressElement import CircularGoalProgressElement
             self._circular = CircularGoalProgressElement(parameter = parameter, parent = self, name = 'Circular')
             return self._circular
+        elif parameterId == 1:
+            from watchFaceParser.models.elements.goalProgress.stepGaugeElement import StepGaugeElement # temp.
+            self._images1 = StepGaugeElement(parameter = parameter, parent = self, name = '?_images?')
+            return self._images1
+        elif parameterId == 2:
+            from watchFaceParser.models.elements.goalProgress.stepGaugeElement import StepGaugeElement # temp.
+            self._images2 = StepGaugeElement(parameter = parameter, parent = self, name = '?_images?')
+            return self._images2
+        elif parameterId == 4:
+            from watchFaceParser.models.elements.goalProgress.stepGaugeElement import StepGaugeElement # temp.
+            self._images4 = StepGaugeElement(parameter = parameter, parent = self, name = '?_images?')
+            return self._images4
         # elif parameterId == 5:
         #     from watchFaceParser.models.elements.goalProgress.circularGoalProgressElement import CircularGoalProgressElement
         #     self._circular2 = CircularGoalProgressElement(parameter = parameter, parent = self, name = 'Circular2')
