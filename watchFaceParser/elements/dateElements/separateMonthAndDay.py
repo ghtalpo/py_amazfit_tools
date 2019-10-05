429d67c48cee2361376691190f7dcdddd4b0bd87
titanzx <tithon5@icloud.com>
2019-10-05 11:13:58 +0700

    Update separateMonthAndDay.py
    Add MonthName

--- watchFaceParser/elements/dateElements/separateMonthAndDay.py
+++ watchFaceParser/elements/dateElements/separateMonthAndDay.py
@@ -1,7 +1,9 @@
 ﻿from watchFaceParser.elements.basicElements.number import Number
+from watchFaceParser.elements.basicElements.imageSet import ImageSet
 
 class SeparateMonthAndDay:
     definitions = {
         1: { 'Name': 'Month', 'Type': Number},
+        2: { 'Name': 'MonthName', 'Type': ImageSet},
         3: { 'Name': 'Day', 'Type': Number},
     }
