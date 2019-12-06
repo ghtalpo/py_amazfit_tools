ce5ad266c9d520b977d03f3caeb00fe7e0bb4338
jjkim <gtalpo@gmail.com>
2019-12-06 22:15:23 +0900

    Add CenterOffset support for AnalogDialFace
    - relative position for each analog hands

---
+++ watchFaceParser/utils/integerConverter.py
@@ -0,0 +1,13 @@
+def uint2int(n):
+    if type(n) == int:
+        if n >= 0x7fffffffffffffff:
+            return -(0xffffffffffffffff - n + 1)
+        if n >= 0x7fffffff:
+            return -(0xffffffff - n + 1)
+    return n
+
+def long2ulong(n):
+    if type(n) == int:
+        if n < 0:
+            return (0xffffffffffffffff + n + 1) & 0xffffffff
+    return n
