5a39a27307e44022d2f2435b22e46927c8b53672
gtalpo <gtalpo@gmail.com>
2020-12-01 01:31:56 +0900

    patch for decompiling Amazfit_RainbowCircle
    - from https://github.com/amazfitbip/py_amazfit_tools

--- watchFaceParser/elements/auxDialFace.py
+++ watchFaceParser/elements/auxDialFace.py
@@ -3,6 +3,7 @@ from watchFaceParser.elements.analogDialFaceElements.clockHand import ClockHand
 class AuxDialFace:
     definitions = {
         1: { 'Name': 'Month', 'Type': ClockHand},
+		2: { 'Name': 'UnknownField2', 'Type': ClockHand},
         3: { 'Name': 'AnalogDOW', 'Type': ClockHand},
     }
 
