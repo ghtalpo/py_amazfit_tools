1279bc0ad710a95addcaac7686a7a4320662cc40
gtalpo <gtalpo@gmail.com>
2020-12-01 03:35:43 +0900

    fix for some WFs
    distance color
    
    - mostly from https://github.com/amazfitbip/py_amazfit_tools

--- watchFaceParser/elements/activityElements/distance.py
+++ watchFaceParser/elements/activityElements/distance.py
@@ -1,9 +1,11 @@
 ﻿from watchFaceParser.elements.basicElements.number import Number
+from watchFaceParser.models.color import Color
 
 class Distance:
     definitions = {
         1: { 'Name': 'Number', 'Type': Number},
         2: { 'Name': 'SuffixImageIndex', 'Type': 'long?'},
         3: { 'Name': 'DecimalPointImageIndex', 'Type': 'long?'},
+        4: { 'Name': 'Color', 'Type': Color},
     }
 
